3db921c5fa44cf8bf7d1ccc3e79bb98132a15e49
Andreas Wilke <andiwilke@gmail.com>
2022-09-15 15:24:26 +0200

    Adding Version 0.2
    New host check and check for if the vulnerability exception already exists.

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ The current script creates a simple Vulnerability Exception for all CVEs found f
 - Info CVEs with a fix
 The Exception will have a filter applied to the host, is not limited to a specific time. The current exception reason is "Fix Pending".
 ## Current limitations (room for improvement)
-- The script does not check if there is already a Vulnerability Exception with the same name.
 - You might see more CVEs applied as filter as you can see inside the UI view. This is due to that a CVE can be affecting multiple packages.
 - The script is in the current version more or less static. We try to make it more optional.
+## Updates
+v02: Adding capability to detect if the host exists, if a vulnerability exception already exists and if yes update the existing one instead of creating a new one.
--- createexceptionforallcves.sh
+++ createexceptionforallcves.sh
@@ -13,12 +13,30 @@ if [ $# -eq 0 ]
 fi
 
 export LW_PROFILE=$LW_PROFILE
-echo "Lacework Vulnerability Exception for a single host (v0.1)"
+echo "Lacework Vulnerability Exception for a single host (v0.2)"
 echo "Get the current CVEs for the Host $HOST"
-getallvulnerabilities=$(lacework api post api/v2/Vulnerabilities/Hosts/search -d '{ "filters": [ { "field": "severity", "expression": "eq", "value": "Medium" }, { "field": "severity", "expression": "eq", "value": "Critical" }, { "field": "severity", "expression": "eq", "value": "High" }, { "field": "severity", "expression": "eq", "value": "Low" }, { "field": "severity", "expression": "eq", "value": "Info" }, { "field": "fixInfo.fix_available", "expression": "eq", "value": "1" }, { "field": "evalCtx.hostname", "expression": "eq", "value": "'$HOST'" } ], "returns": [ "vulnId" ] }')
+getallvulnerabilities=$(lacework api post api/v2/Vulnerabilities/Hosts/search -d '{ "filters": [ { "field": "severity", "expression": "in", "values": ["Medium", "Critical", "High", "Low", "Info"] }, { "field": "fixInfo.fix_available", "expression": "eq", "value": "1" }, { "field": "evalCtx.hostname", "expression": "eq", "value": "'$HOST'" } ], "returns": [ "vulnId" ] }')
+lengthofvulnerabilities=$(echo $getallvulnerabilities | jq 'length')
+if [ $lengthofvulnerabilities -eq 0 ]; then echo "Host not found inside the Lacework environment"; exit 1; else echo "Host found inside the Lacework environment"; fi
 vulnerabilities=$(echo $getallvulnerabilities | jq -r '.data | map(.vulnId) | @csv')
 echo "The following Vulnerabilities have been found:"
 echo $vulnerabilities
-sleep 5
-echo "Adding a new Vulnerability Exception with the current CVEs for the Host $HOST"
-lacework api post api/v2/VulnerabilityExceptions -d '{ "exceptionName": "All CVEs for host '$HOST'", "exceptionReason": "Fix Pending", "vulnerabilityCriteria": { "cve": [ '$vulnerabilities' ] }, "exceptionType": "Host", "props": { "description": "Added all CVEs as exception for the host '$HOST'" }, "resourceScope": { "hostname": [ "'$HOST'" ] } }'
+echo "Check if an Exception Rule already exists..."
+vulnerabilityexception=$(lacework api post api/v2/VulnerabilityExceptions/search -d '{ "filters": [ { "field": "exceptionType", "expression": "eq", "value": "Host" }, { "field": "exceptionReason", "expression": "eq", "value": "Fix Pending" }, { "field": "exceptionName", "expression": "eq", "value": "All CVEs for host '$HOST'" } ]  }')
+lengthofvulnerabilityexception=$(echo $vulnerabilityexception | jq 'length')
+if [ $lengthofvulnerabilityexception -eq 0 ]
+then
+    echo "No Vulnerability Exception found."
+    sleep 5
+    echo "Adding a new Vulnerability Exception with the current CVEs for the Host $HOST"
+    lacework api post api/v2/VulnerabilityExceptions -d '{ "exceptionName": "All CVEs for host '$HOST'", "exceptionReason": "Fix Pending", "vulnerabilityCriteria": { "cve": [ '$vulnerabilities' ] }, "exceptionType": "Host", "props": { "description": "Added all CVEs as exception for the host '$HOST'" }, "resourceScope": { "hostname": [ "'$HOST'" ] } }'
+else
+    echo "Found Vulnerability Exception."
+    sleep 5
+    echo "Updating existing Vulnerability Exception with the current CVEs for the Host $HOST"
+    echo "Getting the Vulnerability Exception GUID..."
+    vulnexceptguid=$(echo $vulnerabilityexception | jq -r '.data[].exceptionGuid')
+    echo "The Vulnerability Exception GUID is $vulnexceptguid"
+    echo "Updating..."
+    lacework api patch api/v2/VulnerabilityExceptions/$vulnexceptguid -d '{ "vulnerabilityCriteria": { "cve": [ '$vulnerabilities' ] } }'
+fi
